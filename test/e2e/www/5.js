df27e0db979dee708542b4ba09a686bdd78e1f9a
jameslin3263 <kinokojiang@hotmail.com>
2021-06-29 20:46:58 +0800

    first commit

---
+++ test/e2e/www/5.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // '@tags': ['index'],
+    'Demo Ruten Index Page': browser => {
+        browser
+        .url('https://www6.vghtpe.gov.tw/reg/c19vaccLine.do')
+        .waitForElementVisible('body')
+        .setValue('input[name=linename]', '')
+        .setValue('input[name=lineid]', '')
+        .setValue('input[name=phone]', '')
+        .execute(function () {
+          document.querySelectorAll("input[value=any]")[0].checked = true
+        }, [])
+        .click('input[type=button]')
+        .pause(100)
+        .saveScreenshot('screenshots/index5.png')
+    }
+  }
